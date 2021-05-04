ff9a437588063c0bf560aa372ee7c58cfcfe14dd
Abraham <nencho_1@hotmail.com>
2021-05-04 11:13:59 +0200

    created proyect and added css atomic design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <i class="fab fa-facebook social-network"></i>
+    <i class="fas fa-hashtag icon-hashtag"></i>
+
+    <h1 class="activitie-title">¿Quienes somos?</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+
+</style>
