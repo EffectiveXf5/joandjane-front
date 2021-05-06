0589a76f21f74706a663b2ba567ceff9e484c3c0
Luisherediaj <73532702+Luisherediaj@users.noreply.github.com>
2021-05-06 14:31:05 +0200

    cambios nombres clases

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,26 @@
 <template>
     <div>
-        <footer class="header">
-            <h1 id="greetings">GREETINGS FROM ASTURIAS</h1>
-            <div class="footer-container">
-                <section>
+        <footer>
+            <h1 id="footer__title">GREETINGS FROM ASTURIAS</h1>
+            <section class="footer__section">
+                <article class="footer__section-about-us">
                     <h3>SOBRE JO&JANE</h3>
                     <p>Términos y condiciones</p>
                     <p>Política de privacidad</p>
                     <p>Política de cookies</p>
                     <p>Aviso legal</p>
-                </section>
-                <section>
+                </article>
+                <article class="footer__section-contact-us">
                     <h3>CONTÁCTANOS</h3>
                     <p>Nuestro horario de att. al cliente es de lunes a domingo de 9 a 20 horas</p>
                     <i class="fas fa-mobile-alt"></i><p>644 184 492</p>
                     <i class="far fa-envelope-open"></i><p>hola@joandjane.es</p>
                     <i class="fas fa-map-marker-alt"></i><p>Coworking Edificio Impulsa Parque<br>Científico-Tecnológico de Gijón CP<br>33203 Asturias (España)</p>
-                </section>
+                </article>
 
 
 
-            </div>
+            </section>
 
         </footer>
     </div>
