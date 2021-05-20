ab31e140b1281683b1d7cdacc0eda02d565ea1ef
Abraham <nencho_1@hotmail.com>
2021-05-20 13:13:49 +0200

    created styles to the home intro

---
+++ src/components/Experiences-home.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <h1>Experiences</h1>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Experiences-home',
+
+}
+</script>
+
+<style>
+
+</style>
--- src/organisms/Home-intro.vue
+++ src/organisms/Home-intro.vue
@@ -1,30 +1,76 @@
 <template>
-    <div>
-    <figure>
-      <img src="@/assets/images/sella1.jpg" alt="imagenPrincipal" class="home-image-cover">
-    </figure>
-    <figure>
-      <img src="@/assets/images/logo amarillo.png" alt="logo" class="logo">
-    </figure>
-      <h1>Reserva las actividades más auténticas de Asturias</h1>
-      <h3>#BookLikeALocal</h3>
-      <Search/>
-    </div>
+    <section class="container">
+      <figure class="container__img-container">
+        <img src="@/assets/images/sella1.jpg" alt="imagenPrincipal">
+      </figure>
+
+      <figure class="container__logo-container">
+        <img src="@/assets/images/logo amarillo.png" alt="logo" class="logo">
+      </figure>
+
+      <article class="container__article">
+        <h1 class="container__article-title">RESERVA LAS ACTIVIDAD<br>MÁS AUTENTICAS DE ASTURIAS</h1>
+        <h3 class="container__article-hasthag"><i class="fas fa-hashtag"></i>BookLikeALocal</h3>
+      </article>
+      <!-- <Search/> -->
+    </section>
 </template>
 
 <script>
-import Search from '../atoms/Search.vue'
+/* import Search from '../atoms/Search.vue' */
 
 export default {
   name: 'Home-intro',
   components: {
-    Search,
+    /* Search, */
   }
 }
 </script>
 
 <style scoped>
-.home-image-cover{
- width:100%;
-}
+
+  .container {
+    height: 70vh;
+    width: 100%;
+  }
+
+  .container__img-container {
+    height: 100%;
+    width: 100%;
+  }
+
+  .container__img-container img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  .container__logo-container img {
+    position: absolute;
+    top: 130px;
+  }
+  
+  .container__article {
+    position: absolute;
+    top: 45%;
+    width: 100%;
+  }
+
+  .container__article-title {
+    font-family: 'Jockey One', sans-serif;
+    font-size: 61px;
+    color: white;
+    text-align: center;
+  }
+
+  .container__article-hasthag {
+    font-size: 2rem;
+    text-align: center;
+  }
+
+  .container__article-hasthag i {
+    color: var(--primary-color);
+    font-size: 2rem;
+  }
+
 </style>
